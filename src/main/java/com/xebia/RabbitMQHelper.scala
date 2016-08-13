79fb68dbc5936d6bf562576953791fab69b49de4
Manish Kumar Thakur <thakur7.manish@gmail.com>
2016-08-13 14:58:39 +0530

    Removed unnecessary method form RabbitMQHelper

--- src/main/java/com/xebia/RabbitMQHelper.scala
+++ src/main/java/com/xebia/RabbitMQHelper.scala
@@ -1,9 +1,9 @@
 package com.xebia
 
-import com.rabbitmq.client.AMQP.BasicProperties
-import com.rabbitmq.client.{Channel, ConnectionFactory, DefaultConsumer, Envelope}
+import com.rabbitmq.client.ConnectionFactory
 
 class RabbitMQHelper(host: String = "localhost", port: Int = 5672) {
+
   def getChannelWithQueue(queue: String) = {
     val factory = new ConnectionFactory()
     factory.setHost(host)
@@ -12,13 +12,5 @@ class RabbitMQHelper(host: String = "localhost", port: Int = 5672) {
     val channel = connection.createChannel()
     channel.queueDeclare(queue, false, false, false, null)
     channel
-  }
-
-  def consumeMessage(channel: Channel) = {
-    val consumer = new DefaultConsumer(channel) {
-      override def handleDelivery(consumerTag: String, envelope: Envelope, properties: BasicProperties, body: Array[Byte]): Unit =
-        println(body.toString)
-    }
-    consumer
   }
 }
